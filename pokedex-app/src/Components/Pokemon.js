24955667918e2e854b0a1b0de8cd90549bebfd27
Dani Pavic <danipavic@Danis-MacBook-Pro.local>
2020-05-05 09:45:23 +0200

    Added dynamic colors for image, button and card background, Added single Pokemon types fetching

--- pokedex-app/src/Components/Pokemon.js
+++ pokedex-app/src/Components/Pokemon.js
@@ -1,30 +1,48 @@
 import React from "react";
 import { Badge, Button, Col } from "react-bootstrap";
+import editColor from "../Utilities/typeColors";
 
 class Pokemon extends React.Component {
   render() {
-    const { id, name } = this.props;
+    const { id, name, types } = this.props;
+    const imageColor = types ? editColor(types[0], "original") : "white";
+    const buttonColor = types ? editColor(types[0], "darken") : "white";
+    const imageColorLight = types ? editColor(types[0], "lighten") : "white";
+    const secondaryColor =
+      types && types.length === 2 ? editColor(types[1], "original") : "white";
+
     return (
       <Col
         sm={12}
         md={5}
-        className="d-flex border border-dark rounded shadow my-2 mx-2"
+        className="d-flex border border-dark rounded shadow my-2 mx-2 px-0"
       >
         <img
+          style={{ backgroundColor: imageColor }}
           className="w-25"
           src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
           alt="pokemon"
         />
-        <div className="w-75 d-flex align-items-center">
+        <div
+          style={{
+            background: `linear-gradient(145deg, ${imageColor} 51%,${
+              secondaryColor === "white" ? imageColorLight : secondaryColor
+            } 51%)`,
+          }}
+          className="w-75 d-flex align-items-center"
+        >
           <div className="d-flex flex-column align-items-start justify-content-center mr-auto">
             <h3 className="text-capitalize mb-0">{`#${id} ${name}`}</h3>
-            <div className="mb-2">
-              <Badge>Type1</Badge>
-              <Badge>Type2</Badge>
+            <div>
+              {types
+                ? types.map((type) => <Badge key={type}>{type}</Badge>)
+                : "loading"}{" "}
             </div>
-            <Button>Stats!</Button>
+            <Button style={{ backgroundColor: buttonColor }}>Stats!</Button>
           </div>
-          <Button style={{ backgroundColor: "red" }}>Catch!</Button>
+          <Button className="mr-3" style={{ backgroundColor: buttonColor }}>
+            Catch!
+          </Button>
         </div>
       </Col>
     );
