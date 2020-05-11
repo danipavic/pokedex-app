78df058c2be165fe4ad9bd2122f94a61400a6ee9
Dani Pavic <danipavic@Danis-MacBook-Pro.local>
2020-05-11 16:58:10 +0200

    Initial modal commit

--- pokedex-app/src/App.js
+++ pokedex-app/src/App.js
@@ -19,35 +19,99 @@ class App extends React.Component {
     this.setState({ pokemon: pokemonObject });
   };
 
-  getPokemonType = async () => {
+  getPokemonData = async () => {
     const { pokemon } = this.state;
+
     Object.keys(pokemon).forEach(async (key) => {
       const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${key}`);
       const data = await response.json();
       const types = [];
+      const abilities = [];
+      let stats = {};
       data.types.forEach((type) => {
-        return types.push(type.type.name);
+        types.push(type.type.name);
       });
-      pokemon[key].types = types;
+
+      data.stats.forEach((stat) => {
+        stats = { ...stats, [stat.stat.name]: stat.base_stat };
+      });
+
+      data.abilities.forEach((ability) => {
+        abilities.push(ability.ability.name);
+      });
+
+      pokemon[key] = {
+        ...pokemon[key],
+        height: data.height,
+        weight: data.weight,
+        types,
+        abilities,
+        stats,
+      };
+
+      this.setState({ pokemon });
+    });
+    Object.keys(pokemon).forEach(async (key) => {
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon-species/${key}`
+      );
+      const data = await response.json();
+
+      pokemon[key] = {
+        ...pokemon[key],
+        species: data.genera[2].genus,
+        description: data.flavor_text_entries[1].flavor_text,
+      };
       this.setState({ pokemon });
     });
   };
 
   async componentDidMount() {
     await this.getAllPokemons();
-    await this.getPokemonType();
+    await this.getPokemonData();
   }
 
   render() {
     const { pokemon, selectedLetter } = this.state;
     return (
-      <Container>
+      <Container
+        className="py-4"
+        fluid
+        style={{ border: "15px solid crimson", backgroundColor: "lightsalmon" }}
+      >
         <Row className="justify-content-center">
           {Object.values(pokemon)
             .filter(({ name }) => name.startsWith(selectedLetter))
-            .map(({ url, name, id, types }) => (
-              <Pokemon key={id} {...{ url, name, id, types }} />
-            ))}
+            .map(
+              ({
+                url,
+                name,
+                id,
+                types,
+                species,
+                description,
+                stats,
+                abilities,
+                height,
+                weight,
+              }) => (
+                <Pokemon
+                  key={id}
+                  {...{
+                    url,
+                    name,
+                    id,
+                    types,
+                    species,
+                    description,
+                    stats,
+                    abilities,
+                    height,
+                    weight,
+                  }}
+                />
+              )
+            )}
         </Row>
       </Container>
     );
